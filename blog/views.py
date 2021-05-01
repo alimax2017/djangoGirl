90398bca0a043babaaa863b1a474c92945a7fe0a
alimax <aslama99@yahoo.com>
2021-05-01 16:58:49 +0200

    oo

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+from django.utils.timezone import now
 from .models import Author
+
 
 # Create your views here.
 def index(request):
-    authors_list= Author.objects.all()
-    return render(request,'index.html',{'authors_list':authors_list})
-    #return render(request,'index.html')
+    authors_list = Author.objects.all()
+    return render(request, 'index.html', {'authors_list': authors_list})
+    # return render(request,'index.html')
+
+
+def homepage(request):
+    # html = "<html><body>it is now : %s .<html><body>" % now()
+    return render(request, 'homepage.html', {'current_time' : now()})
--- djangoGirl/urls.py
+++ djangoGirl/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from blog import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.homepage, name = 'homepage'),
+    path('admin/', admin.site.urls, name = 'admin'),
     path('blog/', include('blog.urls')),
 ]
